b6ff539e6504a97dfb6cb75b01b26c862a43d5ff
CedGrvlDev <cedgrvldev@gmail.com>
2018-01-14 03:34:33 +0100

    Add Header and button to index html and write function in script js

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// fonction permettant d'avoir un chiffre random entre 0 et 2
 function computerPlay( ){
   return Math.floor(Math.random( ) *3);
+};
+// fonction pour un round
+const rock = 0 ;
+const paper = 1;
+const scissors = 2;
+const computerSelection = computerPlay( );
+function playRound (playerSelection, computerSelection ){
+  if (playerSelection == computerSelection){
+    return "It's a Draw";
+  };
+  else if (playerSelection == 0 & computerSelection == 1) {
+    return "You lose, Paper beats Rock";
+  } ;
+  else if (playerSelection == 0 & computerSelection == 1) {
+    return "You win, Rock beats Scissors";
+  };
+  else if (playerSelection == 1 & computerSelection == 0){
+    return "You win, Paper beats Rock";
+  };
+  else if (playerSelection == 1 & computerSelection == 2){
+    return "You lose, Scissors beats Paper ";
+  };
+  else if (playerSelection == 2 & computerSelection == 0){
+    return "You lose, Rock beats Scissors";
+  };
 };
